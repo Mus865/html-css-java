90097a941415b0057b9e7c9f0ee5b76290fc2b4d
mustapha garba <musgab3@gmail.com>
2023-12-16 13:26:36 -0500

    Scope / Description with fix, feat (SemVer) Contributing-to:https://musgab2003.atlassian.net/browse/<JIRA-TICKET> Problem Solution Note

---
+++ 1-HTML-CSS-JavaScript/js/javascript-hello-world.js
@@ -0,0 +1,8 @@
+function getName(){
+    const userName= prompt='what is your name?';
+    sayHello('userName');
+}
+function sayHello() {
+alert('Hello'+ name + '!');
+console.log('Alert sent');
+}
